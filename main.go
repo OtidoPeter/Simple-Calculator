e27e17765d3b684af9613d26312bca01e4d27da1
Otido Peter <peterotido@gmail.com>
2025-05-22 09:15:11 +0300

    more functions

--- main.go
+++ main.go
@@ -19,7 +19,17 @@ func main() {
 	fmt.Println(APP_NAME)
 	fmt.Println(SEPARATOR)
 
-	add := num1 + num2
+	add, multiply, division, subtraction := operations(num1, num2)
+	fmt.Printf("Add: %.2f\n", add)
+	fmt.Println(SEPARATOR)
+	fmt.Printf("Multiply: %.2f\n", multiply)
+	fmt.Println(SEPARATOR)
+	fmt.Printf("Division: %.2f\n", division)
+	fmt.Println(SEPARATOR)
+	fmt.Printf("Subtraction: %.2f\n", subtraction)
+	fmt.Println(SEPARATOR)
+
+	/*add := num1 + num2
 	fmt.Printf("Add: %.2f\n", add)
 	fmt.Println(SEPARATOR)
 
@@ -33,7 +43,7 @@ func main() {
 
 	subtraction := num1 - num2
 	fmt.Printf("Subtraction: %.2f\n", subtraction)
-	fmt.Println(SEPARATOR)
+	fmt.Println(SEPARATOR)*/
 }
 
 func getUserInput(infoText string) float64 {
@@ -41,4 +51,14 @@ func getUserInput(infoText string) float64 {
 	fmt.Print("Give the number: ")
 	fmt.Scan(&userInput)
 	return userInput
+}
+
+func operations(num1, num2 float64) (float64, float64, float64, float64) {
+
+	operationsMultiply := num1 * num2
+	operationsAdd := num1 + num2
+	operationsDiv := num1 / num2
+	operationsSubt := num1 - num2
+	return operationsMultiply, operationsAdd, operationsDiv, operationsSubt
+
 }
