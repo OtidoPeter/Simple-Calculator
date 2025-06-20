36cd77066df5430fa85baaf9416ee3b4126ff2a1
Otido Peter <peterotido@gmail.com>
2025-06-20 06:50:44 +0300

    creating functions

--- main.go
+++ main.go
@@ -11,10 +11,12 @@ func main() {
 	fmt.Println(appName)
 	fmt.Println(separator)
 
-	fmt.Print("Enter your first number here: ")
+	// fmt.Print("Enter your first number here: ")
+	outputText("Enter your first number here: ")
 	fmt.Scan(&numOne)
 
-	fmt.Print("Enter your second number here: ")
+	// fmt.Print("Enter your second number here: ")
+	outputText("Enter your second number here: ")
 	fmt.Scan(&numTwo)
 
 	addition := numOne + numTwo
@@ -25,17 +27,22 @@ func main() {
 
 	subtraction := numOne - numTwo
 
-	/*formattedAdd := fmt.Sprintf("Sum: %.2f\n", addition)
+	formattedAdd := fmt.Sprintf("Sum: %.2f\n", addition)
 	formattedMult := fmt.Sprintf("Product: %.2f\n", multiplication)
 	formattedDiv := fmt.Sprintf("Quotient: %.2f\n", division)
 	formattedSub := fmt.Sprintf("Difference: %.2f\n", subtraction)
-	*/
-	fmt.Printf(`Sum: %.2f\n
+
+	/*fmt.Printf(`Sum: %.2f\n
 
 	Product: %.2f\n
 
 	Quotient: %.2f\n
-	
+
 	Difference: %.2f\n`, addition, multiplication, division, subtraction)
-	// fmt.Print(formattedAdd, formattedMult, formattedDiv, formattedSub)
+	*/
+	fmt.Print(formattedAdd, formattedMult, formattedDiv, formattedSub)
+}
+
+func outputText(text string) {
+	fmt.Print(text)
 }
