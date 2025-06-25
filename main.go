da62eec7422bc4bbd1d2cfbbcebc0c7ab88f8ecf
Otido Peter <peterotido@gmail.com>
2025-06-25 14:34:46 +0300

    more functions

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 	outputText("Enter number here: ")
 	fmt.Scan(&numTwo)
 
-	addition, multiplication, division, subtraction := operations(numOne, numTwo)
+	operations(numOne, numTwo)
 	// addition := numOne + numTwo
 
 	// multiplication := numOne * numTwo
@@ -28,11 +28,6 @@ func main() {
 	// division := numOne / numTwo
 
 	// subtraction := numOne - numTwo
-
-	formattedAdd := fmt.Sprintf("Sum: %.2f\n", addition)
-	formattedMult := fmt.Sprintf("Product: %.2f\n", multiplication)
-	formattedDiv := fmt.Sprintf("Quotient: %.2f\n", division)
-	formattedSub := fmt.Sprintf("Difference: %.2f\n", subtraction)
 
 	/*fmt.Printf(`Sum: %.2f\n
 
@@ -42,18 +37,25 @@ func main() {
 
 	Difference: %.2f\n`, addition, multiplication, division, subtraction)
 	*/
-	fmt.Print(formattedAdd, formattedMult, formattedDiv, formattedSub)
 }
 
 func outputText(text string) {
 	fmt.Print(text)
 }
 
-func operations(numOne, numTwo float64) (add float64, mult float64, div float64, subt float64) {
-	add = numOne + numTwo
-	mult = numOne * numTwo
-	div = numOne / numTwo
-	subt = numOne - numTwo
+func operations(numOne, numTwo float64) (float64, float64, float64, float64) {
+	addition := numOne + numTwo
+	multiplication := numOne * numTwo
+	division := numOne / numTwo
+	subtraction := numOne - numTwo
 
-	return add, mult, div, subt
+	formattedAdd := fmt.Sprintf("Sum: %.2f\n", addition)
+	formattedMult := fmt.Sprintf("Product: %.2f\n", multiplication)
+	formattedDiv := fmt.Sprintf("Quotient: %.2f\n", division)
+	formattedSub := fmt.Sprintf("Difference: %.2f\n", subtraction)
+
+	fmt.Print(formattedAdd, formattedMult, formattedDiv, formattedSub)
+
+	return addition, multiplication, division, subtraction
+	// return
 }
