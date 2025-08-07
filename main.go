9f3d57a5d2f4cd9b800b02a3d7eaa7e0d4bebbdc
Otido Peter <peterotido@gmail.com>
2025-08-07 06:18:11 +0300

    reformatting main.go

--- main.go
+++ main.go
@@ -3,44 +3,124 @@ package main
 import (
 	"errors"
 	"fmt"
+
+	"example.com/project-practice/fileops"
 )
 
-const responseFileName = "response.txt"
+const resultsFileName = "results.txt"
 
-func main() {
-	var numOne float64
-	var numTwo float64
-
-	const appName = "Go Calculator"
-	const separator = "-----------------------"
-
-	fmt.Println(appName)
-	fmt.Println(separator)
-
-	numOne, err := getUserInput("Enter number here: ")
-	if err != nil {
-		fmt.Println(err)
-	}
-	//fmt.Scan(&numOne)
-
-	numTwo, err = getUserInput("Enter number here: ")
-	if err != nil {
-		fmt.Println(err)
-	}
-	//fmt.Scan(&numTwo)
-
-	//fmt.Printf("Sum: %.1f\nDifference: %.1f\nProduct: %.1f\nQuotient: %.1f\n", addition, subtraction, multiplication, division)
-	operations(numOne, numTwo)
-
-}
 func getUserInput(text string) (float64, error) {
 	var userInput float64
 	fmt.Print(text)
 	_, err := fmt.Scan(&userInput)
-
 	if err != nil {
-		return 0, errors.New("Invalid input! Please enter a float.")
+		return 0, errors.New("Only accepts numbers")
 	}
 
 	return userInput, nil
+}
+
+func main() {
+	const appName = "Go Calculator V-12"
+	const separator = "-------------------"
+
+	fmt.Println(appName)
+	fmt.Println(separator)
+
+	operations()
+
+	var choice int
+	fmt.Print("Your choice: ")
+	fmt.Scan(&choice)
+
+	switch choice {
+	case 1:
+		numOne, err := getUserInput("Enter number here: ")
+		//fmt.Scan(&numOne)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		numTwo, err := getUserInput("Enter number here: ")
+		if err != nil {
+			fmt.Println(err)
+		}
+		//fmt.Scan(&numTwo)
+
+		addition := numOne + numTwo
+		//fmt.Printf("Sum: %.1f\n", addition)
+		reformattedAdd := fmt.Sprintf("SUM: %.1f\n", addition)
+		fmt.Print(reformattedAdd)
+		fileops.WriteFloatToFile(resultsFileName, addition)
+		return
+	case 2:
+		numOne, err := getUserInput("Enter number here: ")
+		//fmt.Scan(&numOne)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		numTwo, err := getUserInput("Enter number here: ")
+		//fmt.Scan(&numTwo)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		subtraction := numOne - numTwo
+		//fmt.Printf("DIFFERENCE: %.1f\n", subtraction)
+		reformattedSubt := fmt.Sprintf("DIFFERENCE: %.1f\n", subtraction)
+		fmt.Print(reformattedSubt)
+		fileops.WriteFloatToFile(resultsFileName, subtraction)
+		return
+	case 3:
+		numOne, err := getUserInput("Enter number here: ")
+		//fmt.Scan(&numOne)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		numTwo, err := getUserInput("Enter number here: ")
+		//fmt.Scan(&numTwo)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		multiplication := numOne * numTwo
+		//fmt.Printf("PRODUCT: %.1f\n", multiplication)
+		reformattedMult := fmt.Sprintf("PRODUCT: %.1f\n", multiplication)
+		fmt.Print(reformattedMult)
+		fileops.WriteFloatToFile(resultsFileName, multiplication)
+		return
+	case 4:
+		numOne, err := getUserInput("Enter number here: ")
+		//fmt.Scan(&numOne)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		numTwo, err := getUserInput("Enter number here: ")
+		//fmt.Scan(&numTwo)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		division := numOne / numTwo
+		//fmt.Printf("QUOTIENT: %.1f\n", division)
+		reformattedDiv := fmt.Sprintf("QUOTIENT: %.1f\n", division)
+		fmt.Print(reformattedDiv)
+		fileops.WriteFloatToFile(resultsFileName, division)
+		return
+	case 5:
+		lastResult, err := fileops.GetFloatFromFile(resultsFileName)
+		if err != nil {
+			fmt.Println("ERROR")
+			fmt.Println(err)
+			fmt.Println("---------------")
+		}
+		fmt.Println("Last saved result:", lastResult)
+		return
+	default:
+		fmt.Println("Thank you!")
+		return
+	}
 }
