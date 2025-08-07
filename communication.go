7f4afb67307a67379cde481f2cc7658c828e3183
Otido Peter <peterotido@gmail.com>
2025-08-07 06:19:57 +0300

    separate operations file

---
+++ communication.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func operations() {
+	fmt.Println("What operation do you want?")
+	fmt.Println("1. Addition")
+	fmt.Println("2. Subtraction")
+	fmt.Println("3. Multiplication")
+	fmt.Println("4. Division")
+	fmt.Println("5. Last saved result")
+	fmt.Println("6. Exit")
+}
